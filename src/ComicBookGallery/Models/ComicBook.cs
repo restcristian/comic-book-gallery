3c1c791f6ceaee04be60bed8ec3dcd837fbd5f27
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-07-22 19:41:51 -0400

    Updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,10 +8,28 @@ namespace ComicBookGallery.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public string SeriestTitle { get; set; }
+        public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + "#" + IssueNumber;
+            }
+        }
+        //Series-title-issuenummber.jpg
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                       .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
